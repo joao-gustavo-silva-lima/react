9c316657847abc06d877d5ebdb41cc0300b63577
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-09-22 12:35:36 -0300

    feat(styles): add tailwindcss integration and custom styles
    - integrate tailwindcss for styling in the project
    - create a new CSS file for custom styles and theme variables
    - update main entry point to include the tailwindcss styles
    - modify vite configuration to include tailwindcss plugin

--- pulseflow-dashboard/src/main.tsx
+++ pulseflow-dashboard/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./router/Router";
+import "../src/assets/styles/tailwindcss.style.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
