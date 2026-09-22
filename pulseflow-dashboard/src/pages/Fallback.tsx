d3b1e80e7f8af44e6c4bffb6b223a8acdd60aeb8
joao-gustavo-silva-lima <dev.joaogustavo.dev@gmail.com>
2026-09-22 14:13:09 -0300

    feat(fallback): enhance fallback page layout and styling
    - update main structure for better alignment and centering
    - improve image alt text for better accessibility
    - enhance button styling for improved user experience

--- pulseflow-dashboard/src/pages/Fallback.tsx
+++ pulseflow-dashboard/src/pages/Fallback.tsx
@@ -1,12 +1,20 @@
 export default function Fallback({ message }: { message?: string }) {
   return (
-    <main>
+    <main className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col justify-center items-center gap-gap-lg">
       <img
+        className="w-[75px]"
         src="/public/images/fallback-hero.png"
-        alt="Imagem de uma caixa despejando papeis. Imagem hero para a página de fallback."
+        alt="Imagem de um bloco de notas. Imagem hero para a página de fallback."
       />
-      <p>{message ?? "Oops... Não queremos ficar por aqui."}</p>
-      <a href="/">Voltar ao Dashboard</a>
+      <p className="text-text-secondary text-base text-center">
+        {message ?? "Oops... Não queremos ficar por aqui."}
+      </p>
+      <a
+        className="transition-all duration-[.125s] main-border border-primary font-medium text-primary px-[15px] py-[5px] rounded-md text-nowrap text-center bg-transparent  hover:bg-primary-foreground hover:border-primary-foreground active:bg-primary active:text-primary-foreground"
+        href="/"
+      >
+        Voltar ao Dashboard
+      </a>
     </main>
   );
 }
